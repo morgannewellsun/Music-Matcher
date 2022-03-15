a7f5808af9b8c2e51332655dcdffbb40984f14ce
morgannewellsun <47629346+morgannewellsun@users.noreply.github.com>
2022-03-15 01:08:58 -0400

    Finalized code

---
+++ src/intersection_and_union.py
@@ -0,0 +1,77 @@
+
+import glob
+import os
+import shutil
+
+import pandas as pd
+
+# =============================================================================
+# CONFIG
+# =============================================================================
+
+# directory paths
+dir_a_directory = "D:\\Large Files\\Youtube-DLG\\"
+dir_b_directory = "D:\\Music\\Assorted Weeaboo Music\\"
+output_directory = "D:\\"
+
+# hyperparameters and options
+copy_files = True  # if False, moves the files; if True, copies them
+
+# =============================================================================
+# INTERSECTION AND UNION
+# =============================================================================
+
+# load matches (after they've been checked by the user)
+try:
+    matches_df = pd.read_csv(output_directory + "matches_modified.csv")
+    matches_df = matches_df[~matches_df.isnull().any(axis=1)]
+except FileNotFoundError as error:
+    print("[ERROR] matches_modified.csv was not found. "
+          "Please copy matches.csv to matches_modified.csv, "
+          "manually delete rows with bad matches,"
+          "and then rerun this script.")
+    raise error
+
+# create output directories
+try:
+    os.mkdir(output_directory + "dir_a_unmatched")
+    os.mkdir(output_directory + "dir_b_unmatched")
+    os.mkdir(output_directory + "dir_a_matched")
+    os.mkdir(output_directory + "dir_b_matched")
+except FileExistsError as error:
+    print("[ERROR] Output directories already exist. "
+          "Please manually clean them up and rerun this script.")
+    raise error
+
+# get filepath lists
+dir_a_filepaths = list(glob.glob(dir_a_directory + "*"))
+dir_b_filepaths = list(glob.glob(dir_b_directory + "*"))
+
+# copy/move matched files
+for dir_a_filepath in matches_df["matched_dir_a_filepaths"]:
+    if copy_files:
+        shutil.copy2(dir_a_filepath, output_directory + "dir_a_matched")
+    else:
+        shutil.move(dir_a_filepath, output_directory + "dir_a_matched")
+    dir_a_filepaths.remove(dir_a_filepath)
+for dir_b_filepath in matches_df["matched_dir_b_filepaths"]:
+    if copy_files:
+        shutil.copy2(dir_b_filepath, output_directory + "dir_b_matched")
+    else:
+        shutil.move(dir_b_filepath, output_directory + "dir_b_matched")
+    dir_b_filepaths.remove(dir_b_filepath)
+
+# copy/move unmatched files
+for dir_a_filepath in dir_a_filepaths:
+    if copy_files:
+        shutil.copy2(dir_a_filepath, output_directory + "dir_a_unmatched")
+    else:
+        shutil.move(dir_a_filepath, output_directory + "dir_a_unmatched")
+for dir_b_filepath in dir_b_filepaths:
+    if copy_files:
+        shutil.copy2(dir_b_filepath, output_directory + "dir_b_unmatched")
+    else:
+        shutil.move(dir_b_filepath, output_directory + "dir_b_unmatched")
+
+
+
--- src/main.py
+++ src/main.py
@@ -1,15 +1,15 @@
 
 import glob
 import os
+import pickle
+import time
 
 import librosa
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from scipy.optimize import linear_sum_assignment
 
 # =============================================================================
-# SETUP
+# CONFIG
 # =============================================================================
 
 # directory paths
@@ -17,139 +17,120 @@ dir_a_directory = "D:\\Large Files\\Youtube-DLG\\"
 dir_b_directory = "D:\\Music\\Assorted Weeaboo Music\\"
 output_directory = "D:\\"
 
+# hyperparameters and options
+duration_tolerance_seconds = 0.5
+max_chroma_len = 2**12
+output_all = False  # if False, performs greedy matching; if True, outputs all similarity scores
+
+# =============================================================================
+# PART 0: GET FILE PATHS AND NAMES
+# =============================================================================
+
 # get filepath lists
 dir_a_filepaths = glob.glob(dir_a_directory + "*")
 dir_b_filepaths = glob.glob(dir_b_directory + "*")
-
-# dir_a_filepaths = ["D:\\Large Files\\Youtube-DLG\\Thoughts of a Slayer - KILLING THE KING - by Michal Nitecki _ Epic Hybrid Battle Music.opus"]
-# dir_b_filepaths = ["D:\\Music\\Assorted Weeaboo Music\\Michal Nitecki - KILLING THE KING.mp3", "D:\\Music\\Assorted Weeaboo Music\\MEMODEMO - Digital Comet.mp3"]
-
-dir_a_filepaths = ["D:\\Large Files\\Youtube-DLG\\Raudi - XI - Blue Zenith.opus"]
-dir_b_filepaths = ["D:\\Music\\Assorted Weeaboo Music\\XI - Blue Zenith.mp3", "D:\\Music\\Assorted Weeaboo Music\\Vonikk - Typhoon.mp3"]
 
 # get filename lists
 dir_a_filenames = [os.path.splitext(os.path.split(filepath)[1])[0] for filepath in dir_a_filepaths]
 dir_b_filenames = [os.path.splitext(os.path.split(filepath)[1])[0] for filepath in dir_b_filepaths]
 
+print(f"[INFO] Matching {len(dir_a_filepaths)} files in directory A with {len(dir_b_filepaths)} files in directory B")
+
 # =============================================================================
-# PART 1:
+# PART 1: GET DURATIONS AND CHROMAGRAMS
 # =============================================================================
 
-# construct features and compute durations for first part of all files - this will take time
-dir_a_features = []
+print(f"[INFO] Loading and processing audio files - this will take some time")
+# get chromagrams and durations for files in dir a
+dir_a_chromas = []
+dir_a_durations = []
 for filepath in dir_a_filepaths:
-    y, sr = librosa.load(filepath, mono=True, sr=22050, duration=30)
-    chroma_cens = librosa.feature.chroma_cens(y=y, sr=sr)
-    dir_a_features.append(chroma_cens)
-dir_b_features = []
+    y, sr = librosa.load(filepath, mono=True, sr=22050)
+    dir_a_chromas.append(librosa.feature.chroma_cens(y=y, sr=sr))
+    dir_a_durations.append(librosa.get_duration(y=y, sr=sr))
+dir_a_durations_np = np.array(dir_a_durations)
+# get chromagrams and durations for files in dir b
+dir_b_chromas = []
+dir_b_durations = []
 for filepath in dir_b_filepaths:
-    y, sr = librosa.load(filepath, mono=True, sr=22050, duration=30)
-    chroma_cens = librosa.feature.chroma_cens(y=y, sr=sr)
-    dir_b_features.append(chroma_cens)
+    y, sr = librosa.load(filepath, mono=True, sr=22050)
+    dir_b_chromas.append(librosa.feature.chroma_cens(y=y, sr=sr))
+    dir_b_durations.append(librosa.get_duration(y=y, sr=sr))
+dir_b_durations_np = np.array(dir_b_durations)
+# save to file
+with open(output_directory + "dir_a_chromas.pickle", "wb") as writefile:
+    pickle.dump(dir_a_chromas, writefile)
+with open(output_directory + "dir_b_chromas.pickle", "wb") as writefile:
+    pickle.dump(dir_b_chromas, writefile)
+np.save(output_directory + "dir_a_durations_np.npy", dir_a_durations_np)
+np.save(output_directory + "dir_b_durations_np.npy", dir_b_durations_np)
 
-# construct numpy arrays for features
-# dir_a_features_np = np.array(dir_a_features)
-# dir_b_features_np = np.array(dir_b_features)
-
-D, wp = librosa.sequence.dtw(dir_a_features[0], dir_b_features[0], subseq=True)
-plt.subplot(2, 1, 1)
-plt.title('Database excerpt')
-plt.plot(wp[:, 1], wp[:, 0], label='Optimal path', color='y')
-plt.legend()
-plt.subplot(2, 1, 2)
-plt.plot(D[-1, :] / wp.shape[0])
-plt.xlim([0, dir_a_features[0].shape[1]])
-plt.ylim([0, 2])
-plt.title('Matching cost function')
-plt.tight_layout()
-plt.show()
-
-D, wp = librosa.sequence.dtw(dir_a_features[0], dir_b_features[1], subseq=True)
-plt.subplot(2, 1, 1)
-plt.title('Database excerpt')
-plt.plot(wp[:, 1], wp[:, 0], label='Optimal path', color='y')
-plt.legend()
-plt.subplot(2, 1, 2)
-plt.plot(D[-1, :] / wp.shape[0])
-plt.xlim([0, dir_a_features[0].shape[1]])
-plt.ylim([0, 2])
-plt.title('Matching cost function')
-plt.tight_layout()
-plt.show()
-
-raise Exception
-
-# store feature numpy arrays so we don't have to wait for all that to run again...
-np.save(output_directory + "dir_a_features_np", dir_a_features_np)
-np.save(output_directory + "dir_b_features_np", dir_b_features_np)
+# print("[INFO] Loading previously-computed chromagrams and durations")
+# # load chromagrams and durations from file
+# with open(output_directory + "dir_a_chromas.pickle", "rb") as readfile:
+#     dir_a_chromas = pickle.load(readfile)
+# with open(output_directory + "dir_b_chromas.pickle", "rb") as readfile:
+#     dir_b_chromas = pickle.load(readfile)
+# dir_a_durations_np = np.load(output_directory + "dir_a_durations_np.npy")
+# dir_b_durations_np = np.load(output_directory + "dir_b_durations_np.npy")
 
 # =============================================================================
-# PART 2
+# PART 2: MATCH FILES BASED ON DURATION
 # =============================================================================
 
-# # do the same for durations
-# dir_a_durations = []
-# for filepath in dir_a_filepaths:
-#     y, sr = librosa.load(filepath, mono=True, sr=22050)
-#     dir_a_durations.append(librosa.get_duration(y=y, sr=sr))
-# dir_b_durations = []
-# for filepath in dir_b_filepaths:
-#     y, sr = librosa.load(filepath, mono=True, sr=22050)
-#     dir_b_durations.append(librosa.get_duration(y=y, sr=sr))
-# dir_a_durations_np = np.array(dir_a_durations)
-# dir_b_durations_np = np.array(dir_b_durations)
-# np.save(output_directory + "dir_a_durations_np", dir_a_durations_np)
-# np.save(output_directory + "dir_b_durations_np", dir_b_durations_np)
+print("[INFO] Matching based on duration")
+duration_diffs_np = np.abs(dir_a_durations_np.reshape((-1, 1)) - dir_b_durations_np.reshape((1, -1)))
+duration_matched_indices = list(zip(*np.where(duration_diffs_np <= duration_tolerance_seconds)))
 
 # =============================================================================
-# PART 2
+# PART 3: COMPUTE SIMILARITY USING DYNAMIC TIME WARPING
 # =============================================================================
 
-# load features from file
-dir_a_features_np = np.load(output_directory + "dir_a_features_np.npy")
-dir_b_features_np = np.load(output_directory + "dir_b_features_np.npy")
-dir_a_durations_np = np.load(output_directory + "dir_a_durations_np.npy")
-dir_b_durations_np = np.load(output_directory + "dir_b_durations_np.npy")
+print("[INFO] Computing similarity using dynamic time warping - this will take some time")
+# do dynamic time warp for each pair of duration-matched files
+dtw_costs_np = np.full(shape=(len(dir_a_filepaths), len(dir_b_filepaths)), fill_value=np.inf, dtype=float)
+start_time = time.time()
+for idx_i, (idx_a, idx_b) in enumerate(duration_matched_indices):
+    chroma_a = dir_a_chromas[idx_a]
+    chroma_b = dir_b_chromas[idx_b]
+    chroma_a_subseq_lbound = max(0, int((chroma_a.shape[1] - max_chroma_len) / 2))
+    chroma_a_subseq_ubound = chroma_a_subseq_lbound + max_chroma_len
+    chroma_b_subseq_lbound = max(0, int((chroma_b.shape[1] - max_chroma_len) / 2))
+    chroma_b_subseq_ubound = chroma_b_subseq_lbound + max_chroma_len
+    cost_matrix, warping_path = librosa.sequence.dtw(
+        chroma_a[:, chroma_a_subseq_lbound:chroma_a_subseq_ubound],
+        chroma_b[:, chroma_b_subseq_lbound:chroma_b_subseq_ubound],
+        subseq=True,
+        backtrack=True)
+    dtw_costs_np[idx_a, idx_b] = np.min(cost_matrix[-1, :] / warping_path.shape[0])
+    minutes_remaining = ((time.time() - start_time) * (len(duration_matched_indices) - idx_i - 1) / (idx_i + 1)) / 60
+    # if (idx_i + 1) % 40 == 0:
+    #     print(f"[INFO] Estimated remaining time: {minutes_remaining}")
+np.save(output_directory + "dtw_costs_np.npy", dtw_costs_np)
 
-# compute squared mean differences in chromagrams
-try:  # vectorized, high memory consumption
-    feature_shape = dir_a_features_np.shape[1:3]
-    mean_squared_diffs_np = (
-            dir_a_features_np.reshape((-1, 1, *feature_shape))
-            - dir_b_features_np.reshape((1, -1, *feature_shape)))
-    mean_squared_diffs_np = np.square(mean_squared_diffs_np)
-    mean_squared_diffs_np = np.mean(mean_squared_diffs_np, axis=(2, 3))
-except np.core._exceptions._ArrayMemoryError:  # not vectorized, less memory
-    mean_squared_diffs_np = np.zeros(shape=(len(dir_a_features_np), len(dir_b_features_np)))
-    for idx_a, feature_a_np in enumerate(dir_a_features_np):
-        for idx_b, feature_b_np in enumerate(dir_b_features_np):
-            mean_squared_diffs_np[idx_a, idx_b] = np.mean(np.square(feature_a_np - feature_b_np))
+# # load dynamic time warp similarities from file
+# dtw_costs_np = np.load(output_directory + "dtw_costs_np.npy")
 
-# group by duration (to the nearest second) and find matches
+# =============================================================================
+# PART 4: GREEDILY MATCH FILES
+# =============================================================================
+
+print("[INFO] Performing greedy matching of files")
+duration_matched_indices_np = np.array(duration_matched_indices)
+duration_matched_costs_np = dtw_costs_np[duration_matched_indices_np[:, 0], duration_matched_indices_np[:, 1]]
+duration_matched_indices_sorted_np = duration_matched_indices_np[np.argsort(duration_matched_costs_np)]
 matched_dir_a_indices = []
 matched_dir_b_indices = []
-is_direct_match = []
-dir_a_durations_df = pd.DataFrame({"duration": dir_a_durations_np.astype(int)})
-dir_b_durations_df = pd.DataFrame({"duration": dir_b_durations_np.astype(int)})
-for duration, dir_a_duration_df in dir_a_durations_df.groupby("duration", sort=False):
-    duration_indices_a = dir_a_duration_df.index
-    duration_indices_b = dir_b_durations_df[dir_b_durations_df["duration"] == duration].index
-    duration_mean_squared_diffs_np = mean_squared_diffs_np[duration_indices_a, :][:, duration_indices_b]
-    matched_dir_a_inner_indices, matched_dir_b_inner_indices = linear_sum_assignment(duration_mean_squared_diffs_np)
-    matched_dir_a_indices.extend(list(duration_indices_a[matched_dir_a_inner_indices]))
-    matched_dir_b_indices.extend(list(duration_indices_b[matched_dir_b_inner_indices]))
-    is_direct_match.extend([len(duration_mean_squared_diffs_np.flatten()) == 1] * len(matched_dir_a_inner_indices))
+for idx_a, idx_b in duration_matched_indices_sorted_np:
+    if (idx_a not in matched_dir_a_indices) and (idx_b not in matched_dir_b_indices):
+        matched_dir_a_indices.append(idx_a)
+        matched_dir_b_indices.append(idx_b)
 
-# norm = plt.Normalize()
-# # norm.autoscale(mean_squared_diffs_np)
-# for a, b in zip(matched_dir_a_indices, matched_dir_b_indices):
-#     print(dir_a_filenames[a])
-#     print(dir_b_filenames[b])
-#     print(mean_squared_diffs_np[a, b])
-#     if mean_squared_diffs_np[a, b] > 10000:
-#         plt.imshow(dir_a_features_np[a] - dir_b_features_np[b], norm=norm, interpolation=None)
-#         plt.show()
-#     print("")
+# =============================================================================
+# PART 4: SAVE TO CSV
+# =============================================================================
+
+print("[INFO] Saving results to CSV")
 
 # output matches to csv
 matched_dir_a_filepaths = []
@@ -162,68 +143,12 @@ for idx_a, idx_b in zip(matched_dir_a_indices, matched_dir_b_indices):
     matched_dir_b_filepaths.append(dir_b_filepaths[idx_b])
     matched_dir_a_filenames.append(dir_a_filenames[idx_a])
     matched_dir_b_filenames.append(dir_b_filenames[idx_b])
-    matched_diffs.append(mean_squared_diffs_np[idx_a, idx_b])
+    matched_diffs.append(dtw_costs_np[idx_a, idx_b])
 output_df = pd.DataFrame({
     "matched_dir_a_filepaths": matched_dir_a_filepaths,
     "matched_dir_b_filepaths": matched_dir_b_filepaths,
     "matched_dir_a_filenames": matched_dir_a_filenames,
     "matched_dir_b_filenames": matched_dir_b_filenames,
-    "matched_diffs": matched_diffs,
-    "is_direct_match": is_direct_match})
-output_df = output_df.sort_values(["is_direct_match", "matched_diffs"])
+    "matched_diffs": matched_diffs})
+output_df = output_df.sort_values(["matched_diffs"])
 output_df.to_csv(output_directory + "matches.csv", index=False)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # construct chromagrams and compute durations for first part of all files - this will take time
-# dir_a_features = []
-# dir_a_durations = []
-# for filepath in dir_a_filepaths:
-#     y, sr = librosa.load(filepath, sr=22050, duration=30)
-#     spectro = librosa.feature.melspectrogram(y=y, sr=sr)
-#     spectro = np.max(spectro.reshape((-1, 19)), axis=1).reshape((128, -1))  # max pool time axis with kernel size 19
-#     dir_a_features.append(spectro)
-#     y, sr = librosa.load(filepath, sr=22050)
-#     dir_a_durations.append(librosa.get_duration(y=y, sr=sr))
-# dir_b_features = []
-# dir_b_durations = []
-# for filepath in dir_b_filepaths:
-#     y, sr = librosa.load(filepath, sr=22050, duration=30)
-#     spectro = librosa.feature.melspectrogram(y=y, sr=sr)
-#     spectro = np.max(spectro.reshape((-1, 19)), axis=1).reshape((128, -1))  # max pool time axis with kernel size 19
-#     dir_b_features.append(spectro)
-#     y, sr = librosa.load(filepath, sr=22050)
-#     dir_b_durations.append(librosa.get_duration(y=y, sr=sr))
-#
-# # construct numpy arrays for chromagrams
-# dir_a_features_np = np.array(dir_a_features)
-# dir_b_features_np = np.array(dir_b_features)
-# dir_a_durations_np = np.array(dir_a_durations)
-# dir_b_durations_np = np.array(dir_b_durations)
-#
-# # store chromagram numpy arrays so we don't have to wait for all that to run again...
-# np.save(output_directory + "dir_a_features_np", dir_a_features_np)
-# np.save(output_directory + "dir_b_features_np", dir_b_features_np)
-# np.save(output_directory + "dir_a_durations_np", dir_a_durations_np)
-# np.save(output_directory + "dir_b_durations_np", dir_b_durations_np)
